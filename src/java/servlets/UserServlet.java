5382d9d61b4739a89ce3540289e1d0db545c2e74
Debora Kwon <95396914+Debora-k@users.noreply.github.com>
2022-07-03 20:14:36 -0600

    Found a bug in UserServlet.java

--- src/java/servlets/UserServlet.java
+++ src/java/servlets/UserServlet.java
@@ -30,11 +30,12 @@ public class UserServlet extends HttpServlet {
         String fname = request.getParameter("fname");
         String lname = request.getParameter("lname");
         String password = request.getParameter("password");
-        int roleID = Integer.parseInt(request.getParameter("roleID"));
+        
         
        if(email != null && fname != null && lname != null && password != null) {
            UserService userService = new UserService();
             try {
+                int roleID = Integer.parseInt(request.getParameter("roleID"));
                 userService.insert(email, true, fname, lname, password, roleID);
             } catch (Exception ex) {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
@@ -45,11 +46,12 @@ public class UserServlet extends HttpServlet {
         String fname2 = request.getParameter("fname2");
         String lname2 = request.getParameter("lname2");
         String password2 = request.getParameter("password2");
-        int roleID2 = Integer.parseInt(request.getParameter("roleID2"));
+        
         
        if(email2 != null && fname2 != null && lname2 != null && password2 != null) {
            UserService userService = new UserService();
             try {
+                int roleID2 = Integer.parseInt(request.getParameter("roleID2"));
                 userService.update(email2, true, fname2, lname2, password2, roleID2);
             } catch (Exception ex) {
                 Logger.getLogger(UserServlet.class.getName()).log(Level.SEVERE, null, ex);
